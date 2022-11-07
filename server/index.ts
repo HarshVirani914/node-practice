7225759a7f057662faedc1338d257e8dc4259538
harsh virani <harshvirani.91@gmail.com>
2022-11-07 18:02:32 +0530

    create-express-server

---
+++ server/index.ts
@@ -0,0 +1,18 @@
+// express server running on 3000
+import express from 'express';
+import { json, urlencoded } from 'body-parser';
+import morgan from 'morgan';
+
+const app = express();
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
