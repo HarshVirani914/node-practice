bc51d524a26419f37a066bf632cb7d09e7cb6c79
harsh virani <harshvirani.91@gmail.com>
2022-11-07 13:00:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const data = [
+  {
+    name: "John Doe",
+    age: { years: 32, months: 6 },
+  },
+  {
+    name: "Jane Doe",
+    age: { years: 28, months: 2 },
+  },
+];
+
+data.map((item) => {
+  console.log(item);
+});
+// console.log(Object.entries(data));
